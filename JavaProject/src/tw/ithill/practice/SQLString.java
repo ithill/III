bf31f28f8bb254ff17a23b14ed5eeb7ccaca6af4
MyPlayStation\Dexter <ithillmoon@gmail.com>
2017-04-21 02:36:27 +0800

    0421

---
+++ JavaProject/src/tw/ithill/practice/SQLString.java
@@ -0,0 +1,36 @@
+package tw.ithill.practice;
+
+/* 1.start from "select"
+ * 2.from table
+ * 2-1. space
+ * 2-1. table exist?
+ * 3.space after select before ename and salary(separate from comma)
+ * 4.space before from
+ */
+
+//class SQL{
+	
+//	public void m1(){
+//		String s = "select * from employees";
+//		s1="select";
+//		String s2="select";
+		
+//		if(s1.equalsIgnoreCase(s2) &&s.startsWith("select") )		
+		
+//	}
+//}
+
+
+public class SQLString {
+
+	public static void main(String[] args) {
+		
+		String s = "select ename,salary from employees";
+		if(s.startsWith("select")){
+			if(from table)
+		}
+		
+		
+	}
+
+}
